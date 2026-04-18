57371e6df2079002fbc410bf5f5ebc660eea32f8
prabin <bhusal1008@gmail.com>
2026-04-18 12:30:09 +0545

    first component created

---
+++ 01learning/src/App.jsx
@@ -0,0 +1,12 @@
+// function App() {
+
+//   return <h1> hello world</h1>
+// }
+
+const App = () =>{
+  return(
+    <h1>Hello World</h1>
+  );
+}
+
+export default App;
---
+++ 01learning/src/Grid.jsx
@@ -0,0 +1,7 @@
+const Grid = () =>{
+    return(
+        <h1>first component is created.</h1>
+    );
+}
+
+export default Grid;
